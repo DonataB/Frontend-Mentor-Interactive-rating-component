71d5b2181a55d10428e8ed3819f4a947de203c65
Your Name <donata1989@gmail.com>
2023-02-28 17:11:51 +0100

    Add js logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-// const btn = document.querySelector('#btn');
+const card = document.querySelector('.card');
+const thankYouCard = document.querySelector('.thank-you');
+const submitBtn = document.querySelector('.submit-btn');
+const rating = document.querySelector('#rating');
+const ratingItems = document.querySelectorAll('.rating-btn');
+const back = document.querySelector('.back');
 
-// btn.addEventListener(
-// 	'click',
-// 	() => (window.location.href = 'http://http://127.0.0.1:5500/thankyou.html')
-// );
+function show() {
+	card.style.display = ' none';
+	thankYouCard.style.display = 'block';
+}
+function goback() {
+	card.style.display = 'block';
+	thankYouCard.style.display = 'none';
+}
+
+ratingItems.forEach(rate => {
+	rate.addEventListener('click', () => {
+		rating.innerHTML = rate.innerHTML;
+	});
+});
+
+submitBtn.addEventListener('click', show);
+back.addEventListener('click', goback);
